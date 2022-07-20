f497cc42c7b482cad36a6e604d4f721c797e65d9
Nick_Zur <mr-nicko2011@yandex.ru>
2022-07-20 17:34:04 +0300

    2 task completed

--- src/test/java/ReadFileTests.java
+++ src/test/java/ReadFileTests.java
@@ -1,9 +1,23 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ReadFileTests {
 
     @Test
-    public void secondLineShouldHabAlloha(){
-      //write here second task
+    public void secondLineShouldHaveWordAlloha(){
+        int n = 1; //2 строка
+        try {
+            Path filepath = Paths.get("src/test/resources/ReadFileTests.txt");
+            String line = Files.readAllLines(filepath).get(n);
+            assertEquals("аллоха", line);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
